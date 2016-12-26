8f9c9dccd5260eee8cc2e7d42560b88d662ecff1
rjrhaverkamp <rjrhaverkamp@gmail.com>
2016-12-26 20:56:50 +0100

    made code more readable, secure and python3 ready
    tempfile.mktemp() is deprecated since py3
    
    Remove fname, fout and fin for output_file and readfile using NamedTemporaryFile after use we delete it with unlink.

--- sshmux/ssh.py
+++ sshmux/ssh.py
@@ -3,15 +3,17 @@ import tempfile
 import click
 import socket
 import os.path
+from os import unlink
 
 
 def ssh(host, cmd, user, password, key, timeout=30, bg_run=False):
-    fname = tempfile.mktemp()
-    fout = open(fname, 'w')
+
+    output_file = tempfile.NamedTemporaryFile(delete=False)
     option = []
     option.append("-q")
     option.append("-oStrictHostKeyChecking=no")
     option.append("-oUserKnownHostsFile=/dev/null")
+
     if password != "":
         option.append("-oPubkeyAuthentication=no")
     options = " ".join(option)
@@ -19,68 +21,81 @@ def ssh(host, cmd, user, password, key, timeout=30, bg_run=False):
         options += ' -f'
     ssh_cmd = None
     if password == "":
-        ssh_cmd = 'ssh -i %s %s@%s %s "%s"' % (key, user, host, options, cmd)
+        ssh_cmd = 'ssh -i {0} {1}@{2} {3} "{4}"'.format(
+            key, user, host, options, cmd)
     else:
-        ssh_cmd = 'ssh %s@%s %s "%s"' % (user, host, options, cmd)
+        ssh_cmd = 'ssh {0}@{1} {2} "{3}"'.format(user, host, options, cmd)
+
     child = pexpect.spawn(ssh_cmd, timeout=timeout)
     if password != "":
         child.expect(['password: '])
         child.sendline(password)
-    child.logfile = fout
+
+    child.logfile = output_file
     child.expect(pexpect.EOF)
     child.close()
-    fout.close()
-    fin = open(fname, 'r')
-    stdout = fin.read()
-    fin.close()
+    output_file.close()
+
+    read_file = open(output_file.name, 'r')
+    stdout = read_file.read()
+    output_file.close()
+    read_file.close()
+    unlink(output_file.name)
+
     if 0 != child.exitstatus:
         raise Exception(stdout)
     return stdout
 
 
-def validate_ips(ctx, param, value):
-    for each in value:
+def validate_ip(ctx, param, value):
+    "validate ip addresses"
+    for ip in value:
         try:
-            socket.inet_aton(each)
+            socket.inet_aton(ip)
         except socket.error:
-            raise click.BadParameter('%s - IP address is not valid' % each)
+            raise click.BadParameter(
+                '{0} - IP address is not valid'.format(ip))
     return value
 
 
 def validate_pass(ctx, param, value):
+    """validate password lenght"""
     if len(value) == 0 or len(value) > 100:
         raise click.BadParameter('password length is not valid')
     return value
 
 
 def validate_user(ctx, param, value):
+    """validate username length"""
     if len(value) == 0 or len(value) > 100:
         raise click.BadParameter('username length is not valid')
     return value
 
 
 def validate_key(ctx, param, value):
+    """validate that key exists."""
     if os.path.exists(value):
         return value
     else:
-        raise click.BadParameter('%s file doesn\'t exist' % value)
+        raise click.BadParameter('{0} file doesn\'t exist'.format(value))
 
 
 @click.command()
-@click.option('--ip', '-i', callback=validate_ips, multiple=True, help='IP address')
+@click.option('--ip', '-i', callback=validate_ip, multiple=True, help='IP address')
 @click.option('--username', '-u', callback=validate_user, default='', help='ssh username')
 @click.option('--password', '-p', default='', help='ssh password')
 @click.option('--key', '-k', callback=validate_key, default='', help='ssh private key')
 def sshmux(ip, username, password, key):
-    servers = ip
+    """Open ssh session with each ip and execute a command from stdin."""
     uname = username
     upass = password
     print "Enter your commands below:\n"
     command = raw_input("sshmux > ")
+
     while command != "quit":
-        for each in servers:
-            output = ssh(each, command, uname, upass, key)
-            print each + " : "
+        for server in ip:
+            output = ssh(server, command, uname, upass, key)
+            print server + " : "
             for line in output.split('\n')[1:]:
                 print line
         command = str(raw_input("sshmux > "))
