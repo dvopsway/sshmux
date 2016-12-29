e18c043d0be8ea99451b875100b93936f480e3d7
rjrhaverkamp <rjrhaverkamp@gmail.com>
2016-12-29 18:37:53 +0100

    refactor: minor code cleanups and fix main.py

--- sshmux/validate.py
+++ sshmux/validate.py
@@ -3,9 +3,9 @@ import socket
 from os import path
 
 
-def validate_hostname(ctx, param, value):
+def validate_hostname(ctx, param, hostname):
     "validate hostname / IP address"
-    for address in value:
+    for address in hostname:
         try:
             socket.inet_aton(address)
         except socket.error:
@@ -14,26 +14,26 @@ def validate_hostname(ctx, param, value):
             except socket.gaierror:
                 raise click.BadParameter(
                     '{0} - Address is not valid'.format(address))
-    return value
+    return hostname
 
 
-def validate_pass(value):
+def validate_pass(password):
     """validate password lenght"""
-    if len(value) == 0 or len(value) > 100:
+    if len(password) == 0 or len(password) > 100:
         raise click.BadParameter('password length is not valid')
-    return value
+    return password
 
 
-def validate_user(ctx, param, value):
+def validate_user(ctx, param, username):
     """validate username length"""
-    if len(value) == 0 or len(value) > 100:
+    if len(username) == 0 or len(username) > 100:
         raise click.BadParameter('username length is not valid')
-    return value
+    return username
 
 
-def validate_key(value):
+def validate_key(key_path):
     """validate that key exists."""
-    if path.exists(value):
-        return value
+    if path.exists(key_path):
+        return key_path
     else:
-        raise click.BadParameter('{0} file doesn\'t exist'.format(value))
+        raise click.BadParameter('{0} file doesn\'t exist'.format(key_path))
