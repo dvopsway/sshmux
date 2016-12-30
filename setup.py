195e4ac5473d8b78ab050b4e422202fe96e03f73
Padmakar Ojha <padmakarojha@gmail.com>
2016-12-30 09:13:58 +0530

    Releaseing version 0.2.0

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from setuptools import setup, find_packages
 
-version = '0.1.7'
+version = '0.2.0'
 
 setup(name='sshmux',
       version=version,
       description="Work on multiple machines over ssh at once",
-      long_description="""\
-""",
       classifiers=[],
       keywords='ssh',
       author='Padmakar Ojha',
       author_email='padmakar.ojha@gmail.com',
-	  url='https://github.com/dvopsway/sshmux',
+      url='https://github.com/dvopsway/sshmux',
       license='GPL-3.0',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=["pexpect","ptyprocess","click","future"],
+      install_requires=["pexpect", "ptyprocess", "click", "future"],
       entry_points={
           'console_scripts': [
               'sshmux = sshmux.main:main',
